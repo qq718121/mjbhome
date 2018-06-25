dee78b653f4877a91c6aef4ad0f2325a6c553725
qq718121 <512854577@qq.com>
2018-06-25 16:37:46 +0800

    pact

--- src/components/viewExample/viewExample.vue
+++ src/components/viewExample/viewExample.vue
@@ -1,49 +1,109 @@
 <template>
   <article class="viewExample">
-    <p>请输入示例中<span>红色线框标注</span>的购房合同号</p>
-    <div class="viewText">
-      备案未完成，没有合同号？
-
-    </div>
-    <div class="viewText">
-      请联系官方客服微信：yjjf101，为您开通相关服务。
-
-    </div>
-    <div class="viewExample_img">
-      <img src="/static/view.png" alt="">
+    <p class="viewExample-title viewExample-title1">以下五种购房凭证，您可选择其中的1-3种上传作为审核凭据，请保证资料真实有效。</p>
+    <p class="viewExample-title viewExample-title1">如有任何疑问，请联系官方客服微信：<span class="viewExample-title-span">yyjf101</span>核实
+    </p>
+    <div class="viewExample-img" v-for="(item,num) in imgArr" :key="num">
+      <p>{{item.te}}</p>
+      <div v-if="!preImg" class="viewExample-img-s"><img :src="item.da_sr" alt=""></div>
+      <div v-else class="viewExample-img-s"><img :src="item.sr" alt=""></div>
     </div>
   </article>
 </template>
+<script>
+
+  export default{
+    data(){
+      return {
+        preImg: false,
+        imgArr: [
+          {
+            te: '1.购房合同（商品房买卖合同）：',
+            sr: 'http://oxrgdeqd8.bkt.clouddn.com/%E5%9B%BE%E7%89%87%202.png',
+            da_sr: '/static/view/1.png'
+          },
+          {
+            te: '2.购房发票（购房银行付款凭证）：',
+            sr: 'http://oxrgdeqd8.bkt.clouddn.com/timg.jpg',
+            da_sr: '/static/view/2.png'
+          },
+          {
+            te: '3.购房收据：',
+            sr: 'http://oxrgdeqd8.bkt.clouddn.com/lALPBbCc1eoJZNTNAjrNAaw_428_570.png_620x10000q90g.jpg',
+            da_sr: '/static/view/3.png'
+          },
+          {
+            te: '4.购房协议书（房屋订购协议书）：',
+            sr: 'http://oxrgdeqd8.bkt.clouddn.com/%E5%9B%BE%E7%89%87%203.png',
+            da_sr: '/static/view/4.png'
+          }
+        ]
+      }
+    },
+    created(){
+      this.preload(this.imgArr);
+    },
+
+    methods: {
+
+      preload(Arr = []){
+
+        let len = Arr.length;
+        let count = 0;
+        let this_ = this;
+
+        for (let i = 0; i < len; i++) {
+          let img = new Image();
+          img.onload = function () {
+            count++;
+            if (count == 4) {
+              this_.preImg = true;
+            }
+          };
+          img.src = Arr[i].sr;
+        }
+
+      }
+    }
+  }
+</script>
 <style scoped>
   .viewExample {
-    width: 7.5rem;
+    width: 6.9rem;
     margin: auto;
+    padding: 0.3rem;
   }
 
-  .viewText {
-    text-align: center;
+  .viewExample-title {
+
+    font-family: PingFangSC-Regular;
+    font-size: 0.24rem;
+    letter-spacing: 0.005rem;
+
   }
 
-  .viewExample p {
-    width: 5.04rem;
-    margin: 0.7rem auto 0.3rem;
+  .viewExample-title1 {
+    line-height: 0.34rem;
+    color: #212832;
+  }
+
+  .viewExample-title-span {
+    color: #0080FF;
+  }
+
+  .viewExample-img {
+    margin: 0.4rem auto 0;
+  }
+
+  .viewExample-img p {
     font-family: PingFangSC-Regular;
     font-size: 0.28rem;
     color: #212832;
-    line-height: 0.32rem;
+    line-height: 0.28rem;
   }
 
-  span {
-    color: red;
-  }
-
-  .viewExample_img {
-    width: 6.27rem;
-    margin: 0.4rem auto 1.39rem;
-  }
-
-  .viewExample_img img {
-    width: 100%;
-    height: auto;
+  .viewExample-img-s {
+    width: 6.3rem;
+    margin: 0.2rem auto 0;
   }
 </style>
